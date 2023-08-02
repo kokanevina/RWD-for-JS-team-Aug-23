a9ce6bfda6ef1b371d05039539ce8fdbcfda365c
kokaneva <kokane.vina@gmail.com>
2023-08-02 12:07:29 +0530

    commit

---
+++ AngularWorkspace/ResponsiveUIProject/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css', './myappstyle.css']
+})
+export class AppComponent {
+ // no any development yet
+ trainerName="Vina Patil";
+}
